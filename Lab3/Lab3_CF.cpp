02662bd5b577c17b57bfc1cb2af75dae6b3edf17
Zero-Pix <BT075724@gmail.com>
2024-02-16 20:38:17 -0800

    Lab3 Photo of odd or even

---
+++ Lab3/Lab3_CF.cpp
@@ -0,0 +1,19 @@
+#include <cstdlib>
+#include <iostream>  
+
+int main() {
+    int x =  0;
+    int y =  5;  
+
+    if (x < y){
+        printf("x is less than y\n");  
+    }
+    else if (x %  2 ==  0){
+        printf("x is even\n");  
+    }
+    else{
+        printf("x is odd\n");
+    }
+
+    return  0;
+}
