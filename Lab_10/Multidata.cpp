e1f0eeea455bfc831590122ff27ee7967fbf9fed
Zero-Pix <BT075724@gmail.com>
2024-05-09 13:45:02 -0700

    update

--- Lab_10/Multidata.cpp
+++ Lab_10/Multidata.cpp
@@ -2,10 +2,11 @@
 #include <vector>
 #include <fstream>
 #include <ctime>
+#include <string>
 
 using namespace std;
 
-// Modify vecGen to use templates and take any numeric type vector
+// Function to generate a vector from file data, generic type
 template <typename T>
 void vecGen(const string& filename, vector<T>& v) {
     ifstream file(filename);
@@ -21,7 +22,7 @@ void vecGen(const string& filename, vector<T>& v) {
     }
 }
 
-// Modify iterativeSearch to be templated to handle both int and double
+// Function for iterative search, generic type
 template <typename T>
 int iterativeSearch(const vector<T>& v, T elem) {
     for (size_t i = 0; i < v.size(); i++) {
@@ -32,34 +33,38 @@ int iterativeSearch(const vector<T>& v, T elem) {
     return -1;
 }
 
-// Modify binarySearch to be templated to handle both int and double
+// Function for binary search, generic type
 template <typename T>
 int binarySearch(const vector<T>& v, int start, int end, T elem) {
     if (start > end) {
         return -1;
     }
     int mid = start + (end - start) / 2;
-    if (v[mid] == elem) {
-        return mid;
-    } else if (elem < v[mid]) {
-        return binarySearch(v, start, mid - 1, elem);
+    if constexpr (std::is_same<T, std::string>::value) {
+        // Special handling for string comparison
+        if (v[mid] == elem) {
+            return mid;
+        } else if (v[mid] < elem) {
+            return binarySearch(v, mid + 1, end, elem);
+        } else {
+            return binarySearch(v, start, mid - 1, elem);
+        }
     } else {
-        return binarySearch(v, mid + 1, end, elem);
+        if (v[mid] == elem) {
+            return mid;
+        } else if (elem < v[mid]) {
+            return binarySearch(v, start, mid - 1, elem);
+        } else {
+            return binarySearch(v, mid + 1, end, elem);
+        }
     }
 }
 
-int main(){
+int main() {
     // Testing with integers
-    vector<int> v;
-    v.push_back(1);
-    v.push_back(2);
-    v.push_back(3);
-    v.push_back(4);
-    v.push_back(5);
-    
+    vector<int> v = {1, 2, 3, 4, 5};
     vector<int> elem_to_find = {3, 1, 4};
-
-    for(int i = 0; i < elem_to_find.size(); i++){
+    for (int i = 0; i < elem_to_find.size(); i++) {
         int elem = elem_to_find[i];
         clock_t start = clock();
         int index_found = binarySearch(v, 0, v.size() - 1, elem);
@@ -67,12 +72,11 @@ int main(){
         double elapsed_time_in_sec = (double)(end - start) / CLOCKS_PER_SEC;
         cout << "Index found = " << index_found << "; Time in sec = " << elapsed_time_in_sec << endl;
     }
-    
-    // Testing with doubles
-    vector<double> double_to_find = {3.1, 2.2, 1.3};
-    vector<double> vd = {1.1, 2.2, 3.3, 4.4, 5.5};
 
-    for(int i = 0; i < double_to_find.size(); i++){
+    // Testing with doubles
+    vector<double> vd = {1.1, 2.2, 3.3, 4.4, 5.5};
+    vector<double> double_to_find = {3.1, 2.2, 1.3};
+    for (int i = 0; i < double_to_find.size(); i++) {
         double elem = double_to_find[i];
         clock_t start = clock();
         int index_found = binarySearch(vd, 0, vd.size() - 1, elem);
@@ -80,4 +84,16 @@ int main(){
         double elapsed_time_in_sec = (double)(end - start) / CLOCKS_PER_SEC;
         cout << "Index found = " << index_found << "; Time in sec = " << elapsed_time_in_sec << endl;
     }
+
+    // Testing with strings
+    vector<string> names = {"Al", "Be", "Bea", "Bob", "Cat", "Ci"};
+    string target = "Bea";
+    int result = binarySearch(names, 0, names.size() - 1, target);
+    if (result != -1) {
+        cout << "Found '" << target << "' at index " << result << endl;
+    } else {
+        cout << "Did not find '" << target << "' in the vector." << endl;
+    }
+
+    return 0;
 }
